022f36a747c9ede0a37de5cc47e333fe083b3107
Abdullah Saeed <85762594+AbdullahSaeed1211@users.noreply.github.com>
2023-12-30 10:18:48 +0530

    hero bg

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,17 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    domains: ["github.com", "avatars.githubusercontent.com"],
+  },
   integrations: [tailwind(), icon({
     include: {
       mdi: ["*"],
       // (Default) Loads entire Material Design Icon set
       ic: ["*"] // Loads only Material Design Icon's "account" SVG
     }
-  })],
+  }
+  )],
   output: "server",
   adapter: vercel()
 });
